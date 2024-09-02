686291b4d0667825ab34b2fe207b1a70d68a3c6c
eshaalfatima23 <eshaalfatima23nov@gmail.com>
2024-09-02 21:17:48 +0500

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,9 +6,9 @@ const Banner = () => {
     <div className="bg-green-500 p-3 flex items-center bg-banner bg-no-repeat bg-cover mb-10 justify-evenly">
       <img src={mower} alt="" className="w-[200px] hidden sm:flex" />
       <div className="flex flex-col justify-start items-start  gap-2">
-        <h1 className="text-2xl md:text-4xl font-bold text-white lg:w-[80%] md:w-[100%] ">
+        <h2 className="text-2xl md:text-4xl font-bold text-white lg:w-[80%] md:w-[100%] ">
           We are <span className="text-green-400">100%</span> Electric!
-        </h1>
+        </h2>
 
         <p className="text-xl sm:text-2xl font-light text-white">
           All of our equipment is fully electric for decreased noise and

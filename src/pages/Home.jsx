b30e2dcf9190c674c3f83b42ce4f434a9c2a2033
eshaalfatima23 <eshaalfatima23nov@gmail.com>
2024-09-02 22:46:09 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,7 @@ const Home = () => {
           name="description"
           content="Transform your outdoor space with Regenerative Landscape and Garden, North Carolina’s premier experts in sustainable landscaping, personalized garden design, and professional garden care services."
         />
+        <meta name="google-site-verification" content="Kf8kfrrBuPspakauekp_fFFQyuHcRC3RgpQnipsOC0Q" />
         <title>Regenerative Landscape and Garden: North Carolina's Top Garden Experts</title>
       </Helmet>
       <Hero />
